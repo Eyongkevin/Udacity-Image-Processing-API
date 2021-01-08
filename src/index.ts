b0e1bd564dc3de02a54e31f3cacf3420d7acfbdb
Eyong Kevin <tonyparkerkenz@gmail.com>
2021-01-08 20:07:25 +0100

    chore,style: Configure Prettier
    Configure Prettier to work with Eslint where Eslint defines the code conventions while Prettier performs the auto-formating based on Eslint rules.
    
    This was then applied to format the .ts files.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express';
 
-const app: Application = express()
-const PORT = 5000
+const app: Application = express();
+const PORT = 5000;
 
 // Application routing
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: 'Hello Welcome!' })
-})
+  res.status(200).send({ data: 'Hello Welcome!' });
+});
 
 // Start server
-app.listen(PORT, () => console.log(`Server started on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
